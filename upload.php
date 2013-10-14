910ef6d203b8fe9410347fb64648c8c9db296a93
Tomás Juárez <tom.-95@hotmail.com>
2013-10-14 17:49:31 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8" />
-	<title>Validation test</title>
-</head>
-<body>
-	<div id="form">
-		<form action="receiver.php" method="post" enctype="multipart/form-data">
-			<div><input type="file" name="file" /></div>
-			<div><input type="text" name="title" placeholder="title"/></div>
-			<div><input type="text" name="category" placeholder="category"/></div>
-			<div><input type="submit" value="send!" /></div>
-		</form>
-	</div>
-</body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Validation test</title>
+  </head>
+  <body>
+    <div id="form">
+      <form action="receiver.php" method="post" enctype="multipart/form-data">
+        <div><input type="file" name="file" /></div>
+        <div><input type="text" name="title" placeholder="title"/></div>
+        <div><input type="text" name="category" placeholder="category"/></div>
+        <div><input type="submit" value="send!" /></div>
+      </form>
+    </div>
+  </body>
 </html>
