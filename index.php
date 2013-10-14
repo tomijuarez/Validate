1a8bdaa101c4fe3d5cf917e01dbfdf6649e2a2f5
Tomás Juárez <tom.-95@hotmail.com>
2013-10-14 17:43:32 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $validation->setFlags(array( 'page' => 'int', 'user' => 'nick', 'tab' => 'alpha'
 $passed = $validation->validate($_GET);
 
 if ( !$passed ) {
-	print '<pre>';
-	var_dump($validation->getMessages());
-	print '</pre>';
+  print '<pre>';
+  var_dump($validation->getMessages());
+  print '</pre>';
 }
